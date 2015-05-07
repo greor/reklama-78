18cef0a582e153feba59a4a6be55031eabd7d5c7
gregor <gregor.zoonoid@gmail.com>
2015-05-07 23:58:58 +0300

    Menu limit 8
    Signed-off-by: gregor <gregor.zoonoid@gmail.com>

--- application/classes/controller/front.php
+++ application/classes/controller/front.php
@@ -284,7 +284,7 @@ class Controller_Front extends Controller_Template {
 				->and_where('level', '<', 3)
 				->order_by('level', 'asc')
 				->order_by('position', 'asc')
-				->limit(5)
+				->limit(8)
 				->find_all();
 
 			$this->_menu = $this->_parse_menu_item($pages);
